8359b6471976d105d4e383c097d35a202ccde6f2
Nelimee <adrien.suau@cerfacs.fr>
2022-03-04 18:20:09 +0100

    Fixed typo in Y_constants.Y definition

--- src/sqt/_constants.py
+++ src/sqt/_constants.py
@@ -2,5 +2,5 @@ import numpy
 
 I: numpy.ndarray = numpy.eye(2, dtype=complex)
 X: numpy.ndarray = numpy.array([[0, 1], [1, 0]], dtype=complex)
-Y: numpy.ndarray = 1.0j * numpy.array([[0, 1], [-1, 0]], dtype=complex)
+Y: numpy.ndarray = 1.0j * numpy.array([[0, -1], [1, 0]], dtype=complex)
 Z: numpy.ndarray = numpy.array([[1, 0], [0, -1]], dtype=complex)
