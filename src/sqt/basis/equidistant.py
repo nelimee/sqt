9f20f1ad83cba0d69a5e9400059a83c77f288856
Nelimee <adrien.suau@cerfacs.fr>
2022-03-11 15:25:44 +0100

    Added get_approximately_equidistant_circuits function

--- src/sqt/basis/equidistant.py
+++ src/sqt/basis/equidistant.py
@@ -73,6 +73,31 @@ def point_to_circuit(point: numpy.ndarray, name: str) -> QuantumCircuit:
     return circuit
 
 
+def get_approximately_equidistant_circuits(
+    approximate_point_number: int,
+) -> ty.List[QuantumCircuit]:
+    """Construct and returns circuits that are approximately equidistant.
+
+    This function will construct approximately approximate_point_number
+    quantum circuits that will prepare quantum states that are approximately
+    equidistant when placed on the Bloch sphere.
+
+    :param approximate_point_number: number of circuits that will be generated.
+        This is only an approximation, the actual number of circuits that will
+        be generated might be slightly lower or higher.
+    :return: the generated circuits. Each circuit will have a name that is
+        the list representation of the point it prepares. This point can
+        be retrieved with the following code (yes it is ugly, I am searching
+        for a better alternative):
+            circuits = get_approximately_equidistant_circuits(10)
+            points = [eval(c.name) for c in circuits]
+    """
+    return [
+        point_to_circuit(point, f"{point.tolist()}")
+        for point in get_equidistant_points(approximate_point_number)
+    ]
+
+
 class EquidistantMeasurementBasis(BaseMeasurementBasis):
     """A basis composed of approximately equidistant states on the Bloch sphere.
 
