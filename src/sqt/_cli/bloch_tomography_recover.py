bec2aa05ede795f4a1e31c19e3caca710e1f7da7
Nelimee <adrien.suau@cerfacs.fr>
2022-03-30 20:20:02 +0200

    Improved sqt submission/recovering process

--- src/sqt/_cli/bloch_tomography_recover.py
+++ src/sqt/_cli/bloch_tomography_recover.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import numpy
 
 from qiskit import QuantumCircuit, IBMQ
+from qiskit.providers.aer.jobs.aerjob import AerJob
+from qiskit.providers.aer.jobs.aerjobset import AerJobSet
 from qiskit.providers.ibmq.managed.managedresults import ManagedResults
 from qiskit.result import Result
 from qiskit.providers.ibmq.managed import IBMQJobManager, ManagedJobSet
@@ -28,7 +30,14 @@ _POST_PROCESSING = {
 def unpack_data(
     backup_filename: Path,
 ) -> ty.Tuple[
-    str, ty.List[QuantumCircuit], BaseMeasurementBasis, str, ty.Dict[str, str]
+    str,
+    ty.List[QuantumCircuit],
+    BaseMeasurementBasis,
+    str,
+    ty.Dict[str, str],
+    int,
+    Result,
+    int,
 ]:
     print(f"Recovering data from '{backup_filename}'.")
     with open(backup_filename, "rb") as f:
@@ -39,6 +48,9 @@ def unpack_data(
         data["basis"],
         data["backend_name"],
         data["provider"],
+        data["qubit_number"],
+        data["result"],
+        data["shots"],
     )
 
 
@@ -58,6 +70,7 @@ def main():
         type=str,
         choices=set(_POST_PROCESSING.keys()),
         help="Post-processing method used to reconstruct the density matrices.",
+        nargs="+",
     )
     # parser.add_argument(
     #     "-p",
@@ -68,56 +81,65 @@ def main():
     # )
     args = parser.parse_args()
 
-    post_processing_method: ty.Callable[
-        [Result, QuantumCircuit, BaseMeasurementBasis, int], ty.List[numpy.ndarray]
-    ] = _POST_PROCESSING[args.post_processing_method]
-    job_set_id, raw_circuits, basis, backend_name, provider_data = unpack_data(
-        args.backup_filepath
-    )
+    (
+        job_set_id,
+        raw_circuits,
+        basis,
+        backend_name,
+        provider_data,
+        qubit_number,
+        result,
+        shots,
+    ) = unpack_data(args.backup_filepath)
 
-    if not IBMQ.active_account():
-        print("Loading IBMQ account, this might take some time...")
-        IBMQ.load_account()
-    print("Recovering provider and backend data...")
-    provider = IBMQ.get_provider(**provider_data)
-    backend = provider.get_backend(backend_name)
-    qubit_number: int = backend.configuration().num_qubits
-    print(f"Recovering results from job '{job_set_id}'...")
-    job_manager = IBMQJobManager()
-    job_set: ManagedJobSet = job_manager.retrieve_job_set(job_set_id, provider)
-    managed_result: ManagedResults = job_set.results()
-    results: Result = managed_result.combine_results()
+    results: Result = result
+    if result is None:
+        if not IBMQ.active_account():
+            print("Loading IBMQ account, this might take some time...")
+            IBMQ.load_account()
+        print("Recovering provider and backend data...")
+        provider = IBMQ.get_provider(**provider_data)
+        print(f"Recovering results from job '{job_set_id}'...")
+        job_manager = IBMQJobManager()
+        job_set: ManagedJobSet = job_manager.retrieve_job_set(job_set_id, provider)
+        managed_result: ManagedResults = job_set.results()
+        results = managed_result.combine_results()
 
-    print("Starting post-processing!")
-    # data[qubit_index][i] = (point, density_matrix)
-    data: ty.List[ty.List[ty.Tuple[numpy.ndarray, numpy.ndarray]]] = [
-        list() for _ in range(qubit_number)
-    ]
-    # Might be parallelised.
-    for circuit in raw_circuits:
-        point: numpy.ndarray = numpy.array(eval(circuit.name))  # Quite bad...
-        density_matrices: ty.List[numpy.ndarray] = post_processing_method(
-            results, circuit, basis, qubit_number
+    for post_processing_method_name in args.post_processing_method:
+        print(f"Starting post-processing with '{post_processing_method_name}' method!")
+        post_processing_method: ty.Callable[
+            [Result, QuantumCircuit, BaseMeasurementBasis, int], ty.List[numpy.ndarray]
+        ] = _POST_PROCESSING[post_processing_method_name]
+        # data[qubit_index][i] = (point, density_matrix)
+        data: ty.List[ty.List[ty.Tuple[numpy.ndarray, numpy.ndarray]]] = [
+            list() for _ in range(qubit_number)
+        ]
+        # Might be parallelised.
+        for circuit in raw_circuits:
+            point: numpy.ndarray = numpy.array(eval(circuit.name))  # Quite bad...
+            density_matrices: ty.List[numpy.ndarray] = post_processing_method(
+                results, circuit, basis, qubit_number
+            )
+            for i in range(qubit_number):
+                data[i].append((point, density_matrices[i]))
+
+        backup_filename: str = args.backup_filepath.name
+        backup_dirpath: Path = args.backup_filepath.parent
+        post_process_backup_filename: str = backup_filename.replace(
+            ".pkl", f".post_process_{post_processing_method_name}.pkl"
         )
-        for i in range(qubit_number):
-            data[i].append((point, density_matrices[i]))
-
-    backup_filename: str = args.backup_filepath.name
-    backup_dirpath: Path = args.backup_filepath.parent
-    post_process_backup_filename: str = backup_filename.replace(
-        ".pkl", f".post_process_{args.post_processing_method}.pkl"
-    )
-    post_process_filepath: Path = backup_dirpath / post_process_backup_filename
-    print(f"Saving post-processed data to '{post_process_filepath}'")
-    with open(post_process_filepath, "wb") as f:
-        pickle.dump(
-            {
-                "qubit_number": qubit_number,
-                "density_matrices": data,
-                "backend_name": backend_name,
-                "basis_name": basis.name,
-                "provider": provider_data,
-                "post_processing_method": args.post_processing_method,
-            },
-            f,
-        )
+        post_process_filepath: Path = backup_dirpath / post_process_backup_filename
+        print(f"Saving post-processed data to '{post_process_filepath}'")
+        with open(post_process_filepath, "wb") as f:
+            pickle.dump(
+                {
+                    "qubit_number": qubit_number,
+                    "density_matrices": data,
+                    "backend_name": backend_name,
+                    "basis_name": basis.name,
+                    "provider": provider_data,
+                    "post_processing_method": post_processing_method_name,
+                    "shots": shots,
+                },
+                f,
+            )
--- src/sqt/_cli/bloch_tomography_submit.py
+++ src/sqt/_cli/bloch_tomography_submit.py
@@ -3,8 +3,13 @@ import argparse
 import itertools as it
 import pickle
 from pathlib import Path
+from datetime import datetime
+from qiskit.providers.ibmq.ibmqbackend import IBMQBackend
+
+from rich import print
 
 from qiskit import IBMQ, QuantumCircuit
+from qiskit.providers.aer import AerSimulator
 from qiskit.providers.aer.jobs.aerjob import AerJob
 from qiskit.providers.aer.jobs.aerjobset import AerJobSet
 from qiskit.providers.ibmq import AccountProvider
@@ -12,7 +17,10 @@ from qiskit.providers.ibmq.managed.managedjobset import ManagedJobSet
 
 from sqt.basis.base import BaseMeasurementBasis
 
-from sqt.basis.equidistant import get_approximately_equidistant_circuits
+from sqt.basis.equidistant import (
+    EquidistantMeasurementBasis,
+    get_approximately_equidistant_circuits,
+)
 from sqt.basis.pauli import PauliMeasurementBasis
 from sqt.basis.tetrahedral import TetrahedralMeasurementBasis
 from sqt.circuits import one_qubit_tomography_circuits
@@ -22,6 +30,7 @@ from sqt.passes import compile_circuits
 _BASIS = {
     "pauli": PauliMeasurementBasis(),
     "tetrahedral": TetrahedralMeasurementBasis(),
+    "equidistant": EquidistantMeasurementBasis,
 }
 
 _DEFAULT_SAVE = Path.cwd()
@@ -32,19 +41,26 @@ def get_backup_filename(
     raw_circuits: ty.List[QuantumCircuit],
     backend,
     basis: BaseMeasurementBasis,
-    job: ManagedJobSet,
+    job: ty.Union[ManagedJobSet, AerJob, AerJobSet],
+    delay_dt,
+    shots: int,
 ) -> Path:
+    now = datetime.now()
+    now_str: str = now.isoformat()
+    jobid: str = (
+        job.job_id() if not isinstance(job, ManagedJobSet) else job.job_set_id()
+    )
     filename: str = (
         f"backend-{backend.name()}_basis-{basis.name}_points-{len(raw_circuits)}"
-        f"_jobid-{job.job_set_id()}.pkl"
+        f"_delay-{delay_dt}_shots-{shots}_{now_str}_jobid-{jobid}.pkl"
     )
     path: Path = backup_dir / filename
     if path.is_file():
         path_tmp: Path = backup_dir / (filename + ".tmp")
         print(
-            f"Warning: the file '{path}' already exists on disk! I will save to "
+            f":warning: the file '{path}' already exists on disk! I will save to "
             f"'{path_tmp}' instead to avoid overwriting the file. "
-            "Make sure to secure this file as it might be overwriten in a next run!"
+            "Make sure to secure this file as it might be overwriten in a next run!",
         )
         return path_tmp
     return path
@@ -58,92 +74,141 @@ def backup(
     hub: str,
     group: str,
     project: str,
-    job: ManagedJobSet,
+    job: ty.Union[ManagedJobSet, AerJobSet, AerJob],
+    delay_dt: int,
+    qubit_number: int,
+    shots: int,
 ) -> None:
     backup_filename: Path = get_backup_filename(
-        backup_dir, raw_circuits, backend, basis, job
+        backup_dir, raw_circuits, backend, basis, job, delay_dt, shots
     )
+    jobid: str = (
+        job.job_id() if not isinstance(job, ManagedJobSet) else job.job_set_id()
+    )
+    result = job.result() if isinstance(job, (AerJob, AerJobSet)) else None
     print(f"Backing up in '{backup_filename}'.")
     with open(backup_filename, "wb") as f:
         pickle.dump(
             {
-                "job_set_id": job.job_set_id(),
+                "job_set_id": jobid,
                 "raw_circuits": raw_circuits,
                 "basis": basis,
                 "backend_name": backend.name(),
                 "provider": {"hub": hub, "group": group, "project": project},
+                "delay_dt": delay_dt,
+                "qubit_number": qubit_number,
+                "result": result,
+                "shots": shots,
             },
             f,
         )
 
 
-def get_basis(name: str) -> BaseMeasurementBasis:
+def get_basis(name: str, equidistant_points: ty.Optional[int]) -> BaseMeasurementBasis:
+    if name == "equidistant":
+        if equidistant_points is None:
+            raise RuntimeError(
+                "The '--equidistant-points' option should be set with 'basis=equidistant'."
+            )
+        else:
+            return _BASIS[name](equidistant_points)
     return _BASIS[name]
 
 
-def get_backend(hub: str, group: str, project: str, backend_name: str):
+def _get_ibmq_backend(
+    hub: str,
+    group: str,
+    project: str,
+    backend_name: str,
+) -> IBMQBackend:
     if not IBMQ.active_account():
-        print("Loading IBMQ account, this might take some time...")
+        print(
+            f"Loading IBMQ provider with '{hub}' '{group}' '{project}'. This might take some time..."
+        )
         IBMQ.load_account()
     provider: AccountProvider = IBMQ.get_provider(hub=hub, group=group, project=project)
     potential_backends: ty.List = provider.backends(name=backend_name)
     if len(potential_backends) == 0:
-        raise RuntimeError(
-            f"No backend found with name '{backend_name}'. "
-            "Check that 1) you did not mispelled the backend name "
-            "and 2) the provider you used has access to this backend."
+        print(f"[bold red]No backend found with name '{backend_name}'.[/bold red]")
+        print("[bold orange]Check that:[/bold orange]")
+        print("\t1. You did not mispelled the backend name.")
+        print("\t2. The provider you used has access to this backend.")
+        print(
+            "[bold green]Possible backends:[/bold green]\n\t- ",
+            "\n\t- ".join(b.name() for b in provider.backends()),
+            sep="",
         )
+        exit()
     if len(potential_backends) > 1:
-        raise RuntimeError(
-            f"More than one backend found with name '{backend_name}': "
-            + "["
-            + ", ".join([b.name() for b in potential_backends])
-            + "]"
+        print(
+            f"[bold orange]More than one backend found with name '{backend_name}':[/bold orange]"
         )
+        print("\t- " + "\n\t- ".join(b.name() for b in potential_backends))
+        exit()
 
     return potential_backends[0]
+
+
+def get_backend(
+    hub: str,
+    group: str,
+    project: str,
+    backend_name: str,
+    local_backend: bool,
+    noisy_simulator: bool,
+):
+    if local_backend and not noisy_simulator:
+        return AerSimulator(method="matrix_product_state")
+    else:
+        ibmq_backend = _get_ibmq_backend(hub, group, project, backend_name)
+        if noisy_simulator:
+            return AerSimulator.from_backend(ibmq_backend)
+        else:
+            return ibmq_backend
 
 
 def wait_for_job(managed_jobs: ty.Union[AerJob, AerJobSet, ManagedJobSet]) -> None:
     if isinstance(managed_jobs, ManagedJobSet):
         print(f"Waiting for IBMQ job with ID '{managed_jobs.job_set_id()}'...")
-        print(
-            "Note: you can safely terminate this program (via Ctrl-C or any other mean) "
-            "and wait for the job to finish. You will then be able to recover the tomography "
-            "results with the backup file printed above."
-        )
         try:
             for i, job in enumerate(managed_jobs.jobs()):
                 if job is None:
                     print("One job failed to be submitted...")
                     continue
+                print(
+                    "Note: you can safely terminate this program (via Ctrl-C or any other mean) "
+                    "and wait for the job to finish. You will then be able to recover the tomography "
+                    "results with the backup file printed above."
+                )
                 print(f"Waiting for the completion of job {i}")
                 job.wait_for_final_state()
         except KeyboardInterrupt:
             print(
-                "Interupting this program, the quantum circuit execution is still in "
-                "progress on IBM backend."
+                "\r[orange]Interupting this program, the quantum circuit execution"
+                " is still in progress on IBM backend.[/orange]"
             )
     else:
-        raise NotImplementedError(
-            "This should not happen: only ManagedJobSet should be here."
-        )
+        print(f"[green]Job finished![/green]")
 
 
 def submit_circuits(
-    circuits: ty.List[QuantumCircuit], backend, rep_delay: ty.Optional[float]
-) -> ManagedJobSet:
+    circuits: ty.List[QuantumCircuit],
+    backend,
+    rep_delay: ty.Optional[float],
+    shots: int,
+    delay_dt: int,
+) -> ty.Union[ManagedJobSet, AerJob, AerJobSet]:
     print(f"Compiling the {len(circuits)} circuits that will be submitted.")
     compiled_circuits: ty.List[QuantumCircuit] = compile_circuits(circuits)
-    print(f"Submitting '{len(compiled_circuits)}' circuits.")
-    tags = ["tomography", "bloch"]
+    print(f"Submitting {len(compiled_circuits)} circuits.")
+    tags = ["tomography", "bloch", f"shots={shots}", f"delay={delay_dt}"]
     if rep_delay is not None:
         tags.append(f"rep_delay={rep_delay}")
-    job = submit(compiled_circuits, backend, tags=tags, rep_delay=rep_delay)
+    job = submit(
+        compiled_circuits, backend, tags=tags, rep_delay=rep_delay, shots=shots
+    )
     if isinstance(job, (AerJob, AerJobSet)):
-        raise NotImplementedError(
-            "This should not happen: job should be a ManagedJobSet."
-        )
+        job.wait_for_final_state()
     return job
 
 
@@ -168,8 +233,14 @@ def main():
     parser.add_argument(
         "basis",
         type=str,
-        choices=set(_BASIS.keys()),
+        choices=_BASIS.keys(),
         help="Name of the tomography basis used to perform quantum state tomography.",
+    )
+    parser.add_argument(
+        "--equidistant-points",
+        type=int,
+        default=None,
+        help="If basis is 'equidistant', the number of approximately equidistant projectors that will be used. Else, this option is ignored.",
     )
     parser.add_argument(
         "--hub",
@@ -201,18 +272,64 @@ def main():
         default=None,
         help="Delay between each shot. Default to the backend default value.",
     )
-
+    parser.add_argument(
+        "--shots",
+        type=int,
+        default=None,
+        help="Number of shots performed for each circuit.",
+    )
+    parser.add_argument(
+        "--delay-dt",
+        type=int,
+        default=0,
+        help="Duration (in dt) of the delay to insert before state tomography.",
+    )
+    parser.add_argument(
+        "--max-qubits",
+        type=int,
+        default=None,
+        help="Maximum number of qubits that should be used.",
+    )
+    parser.add_argument(
+        "--local-backend",
+        action="store_true",
+        help="If present, the backend used is a local one.",
+    )
+    parser.add_argument(
+        "--noisy-simulator",
+        action="store_true",
+        help="If present, the given IBMQ backend will be used to initialise a noisy simulator. Implies '--local-backend'.",
+    )
     args = parser.parse_args()
 
-    backend = get_backend(args.hub, args.group, args.project, args.backend)
+    backend = get_backend(
+        args.hub,
+        args.group,
+        args.project,
+        args.backend,
+        args.local_backend,
+        args.noisy_simulator,
+    )
     qubit_number: int = backend.configuration().num_qubits
-    basis: BaseMeasurementBasis = get_basis(args.basis)
-    print(f"Using backend '{backend.name()}'.")
-    print(f"Using basis   '{basis.name}'.")
+    if args.max_qubits is not None:
+        qubit_number = min(qubit_number, args.max_qubits)
+    basis: BaseMeasurementBasis = get_basis(args.basis, args.equidistant_points)
+    print(
+        f"Using {qubit_number} qubit{'s' if qubit_number > 1 else ''} "
+        f"from backend '{backend.name()}'."
+    )
+    print(f"Using basis '{basis.name}'.")
 
     circuits: ty.List[QuantumCircuit] = get_approximately_equidistant_circuits(
         args.approximate_point_number
     )
+    if args.delay_dt > 0:
+        if args.delay_dt % 32 != 0:
+            print(
+                f"[red]:warning:[/red] Delay duration {args.delay_dt} is not a multiple of 32. [orange]Expect issues.[/orange]"
+            )
+        for circuit in circuits:
+            circuit.delay(args.delay_dt, 0, unit="dt")
     tomography_circuits: ty.List[QuantumCircuit] = list(
         it.chain(
             *[
@@ -221,7 +338,14 @@ def main():
             ]
         )
     )
-    job = submit_circuits(tomography_circuits, backend, args.rep_delay)
+    shots: int
+    if args.shots is None:
+        shots = backend.configuration().max_shots
+    else:
+        shots = args.shots
+    job = submit_circuits(
+        tomography_circuits, backend, args.rep_delay, shots, args.delay_dt
+    )
     backup(
         args.backup_dir,
         circuits,
@@ -231,5 +355,8 @@ def main():
         args.group,
         args.project,
         job,
+        args.delay_dt,
+        qubit_number,
+        shots,
     )
     wait_for_job(job)
